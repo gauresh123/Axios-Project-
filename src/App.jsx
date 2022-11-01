ed65a285a69b78a8f1b31cd84d012962957cc21f
gauresh123 <waranggauresh@gmail.com>
2022-11-01 11:20:47 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Axios from './Axios';
+import Home from './Home';
 
 
 function App() {
   return (
     <main>
       <Axios/>
+      <Home/>
     </main>
   );
 }
